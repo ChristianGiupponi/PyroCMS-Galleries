f336b4d6a434c80511d8f1e3572c4416b75233e3
Christian Giupponi <christian.giupponi@stilogo.it>
2014-10-20 09:25:09 +0200

    New method for plugin, now you can get all the categories

--- plugin.php
+++ plugin.php
@@ -48,6 +48,52 @@ class Plugin_galleries extends Plugin
 					),
 				),
 			),
+			'get_galleries' => array(
+				'description' => array(
+					'en' => 'Get all the galleries',
+					'it' => 'Recupera tutte le gallerie'
+				),
+				'single' => false,
+				'double' => true,
+				'variables' => 'galleries_name|galleries_slug|galleries_category|galleries_folder|galleries_cover|galleries_intro|galleries_description|galleries_is_published|galleries_comments_enabled|galleries_seo_title|galleries_seo_keywords|galleries_seo_description',
+				'attributes' => array(					
+					'not_found' => array(
+						'type' => 'string',
+						'flags' => '',
+						'default' => 'Galleries not found',
+						'required' => false
+					),
+				),
+			),
+			'get_categories' => array(
+				'description' => array(
+					'en' => 'Get all the categories',
+					'it' => 'Recupera tutte le categorie'
+				),
+				'single' => false,
+				'double' => true,
+				'variables' => 'galleries_categories_name|galleries_categories_slug',
+				'attributes' => array(					
+					'not_found' => array(
+						'type' => 'string',
+						'flags' => '',
+						'default' => 'You do not have any Category yet',
+						'required' => false
+					),
+					'order_by' => array(
+						'type' => 'string',
+						'flags' => 'created|id',
+						'default' => 'created',
+						'required' => false
+					),
+					'sort' => array(
+						'type' => 'string',
+						'flags' => 'asc|desc|random',
+						'default' => 'asc',
+						'required' => false
+					),
+				),
+			),
 		);
 		
 		return $info;
@@ -83,6 +129,29 @@ class Plugin_galleries extends Plugin
 			'stream' => 'galleries',
 			'namespace' => 'galleries',
 			'where'			=> "`galleries_is_published`='yes'",
+		);
+		
+		$entry = $this->streams->entries->get_entries($params);
+		
+		if( $entry['total'] > 0 )
+		{
+			return $entry['entries'];
+		}
+		
+		return $not_found;
+	}
+	
+	public function get_categories()
+	{
+		$not_found    = ( $this->attribute('not_found') != "" ) ? $this->attribute('not_found') : "You do not have any Category yet.";
+		$order_by 	  = ( $this->attribute('order_by') != "" ) ? $this->attribute('order_by') : "created";
+		$sort 		  = ( $this->attribute('sort') != "" ) ? $this->attribute('sort') : "asc";
+		
+		$params = array(
+			'stream' => 'galleries_categories',
+			'namespace' => 'galleries_categories',
+			'order_by'	=> $order_by,
+			'sort' => $sort
 		);
 		
 		$entry = $this->streams->entries->get_entries($params);
